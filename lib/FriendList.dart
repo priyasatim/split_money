90a990a666a2ca1e511daf5b46812829c6ffa008
priyasatim <priyasatim778@gmail.com>
2024-05-28 21:30:42 +0530

    ui of searchview in add friends and search functionality

--- lib/FriendList.dart
+++ lib/FriendList.dart
@@ -19,6 +19,16 @@ class FriendList extends StatefulWidget {
 }
 
 class FriendListState extends State<FriendList> {
+  List<Map<String, dynamic>> _allUsers = [
+    {"id": 1, "name": "Priya", "email_id": "Priya@gmail.com", "age": 29},
+    {"id": 2, "name": "Abc", "email_id": "Priya@gmail.com", "age": 39},
+    {"id": 3, "name": "Def", "email_id": "Priya@gmail.com", "age": 49},
+    {"id": 4, "name": "ghj", "email_id": "Priya@gmail.com", "age": 59},
+    {"id": 5, "name": "Klm", "email_id": "Priya@gmail.com", "age": 69},
+  ];
+
+  List<Map<String, dynamic>> _filteredUsers = [];
+
   int switcherIndex1 = 0;
   int switcherIndex2 = 0;
   int switcherIndex3 = 0;
@@ -27,6 +37,30 @@ class FriendListState extends State<FriendList> {
   int switcherIndex6 = 0;
   int switcherIndex7 = 0;
 
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredUsers = _allUsers;
+  }
+
+  void _filterUsers(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _allUsers;
+    } else {
+      results = _allUsers
+          .where((user) => user["name"]
+          .toLowerCase()
+          .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _filteredUsers = results;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,28 +68,29 @@ class FriendListState extends State<FriendList> {
         body: SafeArea(
           child: Column(
             children: [
-               Padding(
+              Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: GestureDetector(
-            onTap: () {
-              _showModalBottomSheet(context);
-            },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(Icons.person_add_alt_rounded, color: Colors.black),
-                    Text(
-                      "Friends",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                child: GestureDetector(
+                  onTap: () {
+                    _showModalBottomSheet(context);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person_add_alt_rounded, color: Colors.black),
+                      Text(
+                        "Friends",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                    Icon(Icons.notifications, color: Colors.black),
-                  ],
+                      Icon(Icons.notifications, color: Colors.black),
+                    ],
+                  ),
                 ),
-              ),),
+              ),
               Padding(
                 padding: EdgeInsets.only(left: 16.0, top: 12.0),
                 child: Row(
@@ -88,47 +123,49 @@ class FriendListState extends State<FriendList> {
               ),
               SizedBox(height: 20),
               Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[200], // Background color
-            borderRadius: BorderRadius.circular(25), // Optional: You can adjust the border radius as needed
-          ),
-          child: SlideSwitcher(
-                children: [
-                  Text(
-                    'Overall',
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: switcherIndex4 == 0
-                            ? Colors.black.withOpacity(0.9)
-                            : Colors.grey),
-                  ),
-                  Text(
-                    'I owe',
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: switcherIndex4 == 1
-                            ? Colors.black.withOpacity(0.9)
-                            : Colors.grey),
-                  ),
-                  Text(
-                    'Owns me',
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: switcherIndex4 == 2
-                            ? Colors.black.withOpacity(0.9)
-                            : Colors.grey),
-                  ),
-                ],
-                onSelect: (int index) => setState(() => switcherIndex4 = index),
-                containerColor: Colors.white24,
-                indents: 9,
-                containerHeight: 50,
-                containerWight: 350,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200], // Background color
+                  borderRadius: BorderRadius.circular(
+                      25), // Optional: You can adjust the border radius as needed
+                ),
+                child: SlideSwitcher(
+                  children: [
+                    Text(
+                      'Overall',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: switcherIndex4 == 0
+                              ? Colors.black.withOpacity(0.9)
+                              : Colors.grey),
+                    ),
+                    Text(
+                      'I owe',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: switcherIndex4 == 1
+                              ? Colors.black.withOpacity(0.9)
+                              : Colors.grey),
+                    ),
+                    Text(
+                      'Owns me',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: switcherIndex4 == 2
+                              ? Colors.black.withOpacity(0.9)
+                              : Colors.grey),
+                    ),
+                  ],
+                  onSelect: (int index) =>
+                      setState(() => switcherIndex4 = index),
+                  containerColor: Colors.white24,
+                  indents: 9,
+                  containerHeight: 50,
+                  containerWight: 350,
+                ),
               ),
-        ),
               SizedBox(height: 10),
               CustomColumn(),
             ],
@@ -137,22 +174,119 @@ class FriendListState extends State<FriendList> {
       ),
     );
   }
+
+  void _showModalBottomSheet(BuildContext context) {
+    double totalHeight =
+        _allUsers.length * 56.0; // Height of each list item
+    double deviceHeight = MediaQuery.of(context).size.height;
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: totalHeight > deviceHeight,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+            child : Container(
+          width: MediaQuery.of(context).size.width,
+          height: 200,
+          padding: EdgeInsets.only(top: 12, bottom: 12),
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              CenterDot(
+                width: 200,
+                color: Colors.grey,
+              ),
+              const Text(
+                'Add Friend',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              SizedBox(height: 5),
+              Container(
+                margin: EdgeInsets.only(left: 16.0,right: 16.0),
+                child : TextField(
+                  onChanged: (value) => _filterUsers(value),
+                  decoration: const InputDecoration(
+                      labelText: 'Search',
+                      suffixIcon: Icon(Icons.arrow_right_alt_rounded),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)))),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _filteredUsers.length,
+                    itemBuilder: (context, index) => Container(
+                        key: ValueKey(_filteredUsers[index]["id"]),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                            AssetImage('assets/images/profile.png'),
+                          ),
+                          title: Text(
+                            _filteredUsers[index]['name'],
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14),
+                          ),
+                          subtitle: Text(
+                            _filteredUsers[index]['email_id'],
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12),
+                          ),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {
+                                  // Define the text's action here
+                                  print(
+                                      'Text pressed for user: ${_filteredUsers[index]['name']}');
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child:  Text(
+                                    'Add Friend',
+                                    style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold, fontSize: 12),
+                                  ),
+
+                                ),
+                              ),
+                            ],
+                          ),
+                        ))),
+              ),
+            ],
+            ),
+            ),
+        );
+      },
+    );
+  }
+
 }
 
-
 class CustomColumn extends StatelessWidget {
-  final List<String> items = List<String>.generate(100, (index) => "Item $index");
+  final List<String> items =
+      List<String>.generate(100, (index) => "Item $index");
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: SingleChildScrollView(
+    return Expanded(
+        child: SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: items.map((item) {
           return CustomListItem(
             title: item,
             subtitle: '02 Feb 2024',
-            imageUrl: 'https://via.placeholder.com/150', // Example image URL
+            imageUrl: 'https://via.placeholder.com/150',
+            // Example image URL
             ownAmount: '322',
             onTap: () {
               // Do something when item is tapped
@@ -223,28 +357,26 @@ class CustomListItem extends StatelessWidget {
               ),
             ),
             const Positioned(
-              child : Column(
-                children: [
-                  Text(
-                    'you owe', // Add your additional text here
-                    style: TextStyle(
-                      color: Colors.grey, // Adjust color as needed
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                child: Column(
+              children: [
+                Text(
+                  'you owe', // Add your additional text here
+                  style: TextStyle(
+                    color: Colors.grey, // Adjust color as needed
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold,
                   ),
-
-                  Text(
-                    '-₹124', // Add your additional text here
-                    style: TextStyle(
-                      color: Colors.red, // Adjust color as needed
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                Text(
+                  '-₹124', // Add your additional text here
+                  style: TextStyle(
+                    color: Colors.red, // Adjust color as needed
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              )
-            )
+                ),
+              ],
+            ))
           ],
         ),
       ),
@@ -252,49 +384,6 @@ class CustomListItem extends StatelessWidget {
   }
 }
 
-void _showModalBottomSheet(BuildContext context) {
-  List<Map<String,dynamic>> _allUsers =[
-    {"id":1, "name":"Priya","email_id": "Priya@gmail.com","age":29},
-    {"id":2, "name":"Abc","email_id": "Priya@gmail.com","age":39},
-    {"id":3, "name":"Def","email_id": "Priya@gmail.com","age":49},
-    {"id":4, "name":"ghj","email_id": "Priya@gmail.com","age":59},
-    {"id":5, "name":"Klm","email_id": "Priya@gmail.com","age":69},
-  ];
-  showModalBottomSheet(
-    context: context,
-    builder: (BuildContext context) {
-      return Container(
-        width: MediaQuery.of(context).size.width,
-        height: 200,
-        padding : EdgeInsets.only(top: 12,bottom: 12),
-        color: Colors.white,
-        child:  Column(
-          children: <Widget>[
-            CenterDot(
-              width: 200,
-              color: Colors.grey,
-            ),
-            Text('Add Friend', style: TextStyle(fontWeight: FontWeight.bold),),
-            Text('Priya@gmail.com', style: TextStyle(fontSize: 14)),
-            Text('Priya@gmail.com', style: TextStyle(fontSize: 12),),
-            Expanded(
-          child: ListView.builder(
-                itemCount: _allUsers.length,
-                itemBuilder: (context, index) => Card(
-                    key : ValueKey(_allUsers[index]["id"]),
-                    elevation: 4,
-                    child :ListTile(
-                      title: Text(_allUsers[index]['name'],style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 14),),
-                      subtitle: Text(_allUsers[index]['email_id'],style: TextStyle(color: Colors.grey,fontSize: 12),),
-                    )
-                )
-            ),),
-          ],
-        ),
-      );
-    },
-  );
-}
 
 class CenterDotPainter extends CustomPainter {
   final Color color;
@@ -341,5 +430,4 @@ class CenterDot extends StatelessWidget {
     );
   }
 }
-
 
